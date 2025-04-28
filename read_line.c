d3f95a94f546b8ec0fd313f507c9a8d2526c20a0
rye-to-rye <ryan.rahman.college@gmail.com>
2025-04-27 20:16:55 -0400

    Update read_line.c

--- read_line.c
+++ read_line.c
@@ -1,19 +1,17 @@
-#include <stdio.h>
-#include <ctype.h>
-#include "read_line.h"
+int read_line(char str[], int n) { 
+    int ch, i = 0;
 
-int read_line(char str[], int n) //read in a string
-{
-  int ch, i = 0;
+    while (isspace(ch = getchar()))
+        ;  
 
-  while (isspace(ch = getchar()))
-    ;
-  str[i++] = ch;
-  while ((ch = getchar()) != '\n' && ch != EOF) {
-    if (i < n)
-      i++;
-    str[i++] = ch;
-  }
-  str[i] = '\0';
-  return i;
+    if (ch != '\n' && ch != EOF) {  
+        str[i++] = ch;
+    }
+
+    while ((ch = getchar()) != '\n' && ch != EOF) {
+        if (i < n)
+            str[i++] = ch;
+    }
+    str[i] = '\0';
+    return i;
 }
